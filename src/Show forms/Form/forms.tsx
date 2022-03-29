045cd20b45720ac303a0ac7606c63394bc136b50
sonirutvi <rutvis@webelight.co.in>
2022-03-29 18:33:19 +0530

    March29

--- src/Show forms/Form/forms.tsx
+++ src/Show forms/Form/forms.tsx
@@ -1,42 +1,55 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from 'yup';
 
 function Forms() {
 
-    const formInitialSchema = {
-        firstName: '',
-        email: ''
-    }
+
 
     const formValidationSchema = Yup.object().shape({
         firstName: Yup.string().required('Name is required'),
         email: Yup.string().required('email is required').email("Please enter valid email")
     })
 
-    const handleFormSubmit = (values: any) => {
-        console.log("Submitted values", values)
-    }
+    
 
     return (
         <>
 
-            <Formik initialValues={formInitialSchema}
+            <Formik initialValues={{ firstName: "", email: '' }}
                 validationSchema={formValidationSchema}
-                onSubmit={values => handleFormSubmit(values)}>
-                <Form>
-                    <div>
-                        <Field type="text" name="firstName" placeholder="Enter your Name" />
-                        <p className="text-danger"><ErrorMessage name="firstName"/></p>
-                    </div>
-                    <div>
-                        <Field type="text" name="email" placeholder="Enter your email" />
-                        <p className="text-danger"><ErrorMessage name="email"/></p>
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-primary">Submit values</button>
-                    </div>
-                </Form>
+                onSubmit={(values,{setSubmitting, resetForm})=>{
+                    setSubmitting(true);
+
+                    setTimeout(() => {
+                       alert(JSON.stringify(values,null,2));
+                       resetForm();
+                       setSubmitting(false)
+                    },500);
+                }}
+            >
+                {({ values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <div>
+                            <Field type="text" value={values.firstName} onBlur={handleBlur} name="firstName" placeholder="Enter your Name" onChange={handleChange} />
+                            <p className="text-danger"><ErrorMessage name="firstName" /></p>
+                        </div>
+                        <div>
+                            <Field type="text" name="email" placeholder="Enter your email" />
+                            <p className="text-danger"><ErrorMessage name="email" /></p>
+                        </div>
+                        <div>
+                            <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Submit values</button>
+                        </div>
+                    </Form>
+                )}
+
             </Formik>
 
         </>
